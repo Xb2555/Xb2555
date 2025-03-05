4945161e1eb77ceb9dfe050e26fe136e3033557d
Xb2555 <2012753288@qq.com>
2025-03-06 00:02:57 +0800

    feat: 完成文件内容读取功能

--- 3123004787/src/main/java/com/gg/Main.java
+++ 3123004787/src/main/java/com/gg/Main.java
@@ -1,5 +1,9 @@
 package com.gg;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         if (args.length != 3) {
@@ -13,5 +17,18 @@ public class Main {
         //答案文件地址
         String outputPath = args[2];
 
+        //读取原文
+    }
+
+    // 读取文件内容
+    private static String readFile(String filePath) throws IOException {
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        }
+        return content.toString().trim();
     }
 }
